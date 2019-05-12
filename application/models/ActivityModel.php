6bdf83cd975a6a5bc85772371522c1ec8db00a26
BowearpCT <c.kunapakornkarn@hotmail.com>
2019-05-12 11:29:43 +0700

    add database on config file

--- application/models/ActivityModel.php
+++ application/models/ActivityModel.php
@@ -3,7 +3,7 @@
 class ActivityModel extends CI_Model{
     public function insert($data)
     {
-        $this->db->insert('user', $data);
+        $this->db->insert('activity', $data);
         return ($this->db->affected_rows() != 1) ? false : true;
     }
     public function update($data)
