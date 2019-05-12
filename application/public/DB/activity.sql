112255ed8a10dd4d669c5baf071efd71b2230808
arifeen008 <feenkundee@gmail.com>
2019-05-12 10:59:30 +0700

    add database activity

---
+++ application/public/DB/activity.sql
@@ -0,0 +1,62 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.4
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: May 12, 2019 at 05:40 AM
+-- Server version: 10.1.37-MariaDB
+-- PHP Version: 5.6.40
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `activity`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `activity`
+--
+
+CREATE TABLE `activity` (
+  `id_activity` int(8) NOT NULL,
+  `name_activity` varchar(255) CHARACTER SET utf8 NOT NULL,
+  `detail_activity` varchar(255) CHARACTER SET utf8 NOT NULL,
+  `status` varchar(255) CHARACTER SET utf8 NOT NULL,
+  `start_activity` date NOT NULL,
+  `end_activity` date NOT NULL,
+  `time_activity` int(11) NOT NULL,
+  `student_activity` varchar(11) CHARACTER SET utf8 NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `activity`
+--
+
+INSERT INTO `activity` (`id_activity`, `name_activity`, `detail_activity`, `status`, `start_activity`, `end_activity`, `time_activity`, `student_activity`) VALUES
+(0, '', '0', '0', '0000-00-00', '0000-00-00', 0, '0');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `activity`
+--
+ALTER TABLE `activity`
+  ADD PRIMARY KEY (`id_activity`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
